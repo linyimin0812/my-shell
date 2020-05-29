5ccbce44a31a89a1cd33ef08e714673654a7b480
linyimin-bupt <linyimin520812@gmail.com>
2020-05-29 23:46:05 +0800

    optimize get all crontab command

--- 获取crontab命令信息.sh
+++ 获取crontab命令信息.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo grep "CRON" /var/log/syslog | awk 'BEGIN {print "["} {printf "time: %s %s %s, user: %s, commnad: %s,\n", $1,$2,$3, $6, $8;} END {print "]"}'
+sudo grep "CRON" /var/log/syslog | awk 'BEGIN {print "["} {printf "{\"time\": \"%s,%s ,%s\", \"user\": \"%s\", \"command\": \"", $1,$2,$3,$6;for(i=8;i<=NF;i++) printf $i""FS;print "\"},"} END {print "]"}' |sed 's/[\(\)]//g' | sed 'N;$s/,\n/\n/;P;D';
